e5ef74f00cc24e24c83162683d65c4d90ac52bda
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-28 21:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let a = prompt("Enter First Number");
+let b = prompt("Enter Second Number");
+a = parseInt(a);
+b = parseInt(b);
+if(a === b ){
+	alert(true);
+}
+else{
+	alert(false);
+}
